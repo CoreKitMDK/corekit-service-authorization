d9864f039f29b8fabb7536d7bf9b7b0784fbbc9a
luciantin <lord66tin66@gmail.com>
2025-06-29 16:35:28 +0000

    Updated functions

--- cmd/rights-give/handle_test.go
+++ cmd/rights-give/handle_test.go
@@ -14,8 +14,6 @@ import (
 	"github.com/google/uuid"
 )
 
-// TestHandle ensures that Handle executes without error and returns the
-// HTTP 200 status code indicating no errors.
 func TestHandle(t *testing.T) {
 	entityID := uuid.New()
 	resourceID := uuid.New()
